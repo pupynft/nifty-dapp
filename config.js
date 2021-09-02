4adcc12dd832fafb506e5c30e0ff068d92d41813
BitBD <info@kittytoken.io>
2021-09-02 12:51:30 +0600

    PuppyNFT

---
+++ config.js
@@ -0,0 +1,2 @@
+export const nftmarketaddress = "0x459cce23cdCFcc8983237868F26c863054c5Ff0d"
+export const nftaddress = "0x6dD250B30325A4A4AE5907A7f3cA99b82D74D402"
---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+const  fs = require('fs-extra');
+const JsonRpcProvider = require('@ethersproject/providers');
+const Wallet = require('@ethersproject/wallet');
+
+
+const hre = require("hardhat");
+
+
+async function main() {
+  const NFTMarket = await hre.ethers.getContractFactory("PuppyNFTMarket");
+  const nftMarket = await NFTMarket.deploy();
+  await nftMarket.deployed();
+  console.log("nftMarket deployed to:", nftMarket.address);
+
+  const NFT = await hre.ethers.getContractFactory("PNFT");
+  const nft = await NFT.deploy(nftMarket.address);
+  await nft.deployed();
+  console.log("nft deployed to:", nft.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
